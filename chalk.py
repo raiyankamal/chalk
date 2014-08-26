ce0b4910162b4d05d100e5e53079870128c56c5c
Raiyan Kamal <raiyan.kamal@gmail.com>
2014-08-26 01:01:54 -0400

    rectangle for each item

--- chalk.py
+++ chalk.py
@@ -10,7 +10,12 @@ def draw_ds(ds, x=0.0, y=0.0, dwg=None):
 
 	if type(ds) is list:
 		for item in ds:
+			px,py = (x,y)
+
 			x,y = draw_ds(item, x, y, dwg)
+
+			dwg.add(dwg.rect(insert=(px, py), size=(10.0, y-py), fill='rgb(50,50,50)'))
+
 	else:
 		print('drawing %s' % ds)
 		dwg.add(dwg.text(str(ds), insert=(x, y), fill='red'))
