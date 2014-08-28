57a32cde388dafd47344cf769a98a254fde38913
Raiyan Kamal <raiyan.kamal@gmail.com>
2014-08-27 20:05:23 -0400

    Code snippets for testing list, list of list, list of strings, and int

--- test.py
+++ test.py
@@ -1,4 +1,29 @@
+import random
 import chalk
 
-L = [ l*2 for l in range(0,123)]
-chalk.draw(L)
+L = [ x/2 if x%2==0 else x*3+1 for x in range(0,12)]
+# chalk.draw(L)
+
+LL = [ [ m for m in range(2*l, 2*l+3) ] for l in range(0,12)]
+# chalk.draw(LL)
+
+names = [
+'Kein Pardon',
+'Kein Platz fr Gerold',
+'Kein Sex ist auch keine Lsung',
+'Keine Angst Liebling, ich pass schon auf',
+'Keiner hat das Pferd geksst',
+'Keiner liebt mich',
+'Keinohrhasen',
+'Keiros Cat',
+'La Prima Donna',
+'La Primeriza',
+'La Prison De Saint-Clothaire',
+'La Puppe',
+'La Pjara',
+'La Pergola de las Flores',
+]
+#chalk.draw(names)
+
+x = random.randint(0,1000)
+# chalk.draw(x)
